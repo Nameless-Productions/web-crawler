4c79da3c780d241aa62e816e8db8f24c0d807fe5
thenamelessdev <nameless@thenamelessdev.com>
2026-07-08 10:43:45 +0200

    feat: ability to craw, doesn't store text yet

--- src/getRobots.ts
+++ src/getRobots.ts
@@ -1,10 +1,15 @@
 import { createRequire } from 'module';
+import { usrAgent } from './index.js';
 const require = createRequire(import.meta.url);
 const robotsParser = require('robots-parser');
 
 export async function getRobots(baseURL: string) {
     const robotsURL = String(new URL("/robots.txt", baseURL));
-    const res = await fetch(robotsURL);
+    const res = await fetch(robotsURL, {
+        headers: {
+            "User-Agent": usrAgent
+        }
+    });
     const bodyText = res.ok ? await res.text() : "";
 
     const robots = robotsParser(robotsURL, bodyText);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { getRobots } from "./getRobots.js";
 import { readWebsites } from "./readWebsites.js";
+import * as cheerio from "cheerio"
 
-const usrAgent = "webCrawler/1.0";
+export const usrAgent = "webCrawler/1.0";
 
 (async () => {
     const websites = await readWebsites();
@@ -12,7 +13,17 @@ const usrAgent = "webCrawler/1.0";
         const robots = await getRobots(website);
         
         if(robots.isAllowed(website, usrAgent)) {
-            // crawl website
+            const websiteHtml = await fetch(website, {
+                headers: {
+                    "User-Agent": usrAgent
+                }
+            });
+            const html = websiteHtml.ok ? await websiteHtml.text() : "";
+
+            const $ = cheerio.load(html);
+            $("script, style, noscript").remove()
+            const htmlText = $("body").text().trim()
+            console.log("Succesfuly crawled ", website)
         } else {
             console.log("Not allowed to craw ", website)
         }
